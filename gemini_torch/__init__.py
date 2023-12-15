0576885ca92f2be856ac0ce1b79edbb4fd65a301
Eternal Reclaimer <98760976+kyegomez@users.noreply.github.com>
2023-12-14 20:51:56 -0800

    Update __init__.py

--- gemini_torch/__init__.py
+++ gemini_torch/__init__.py
@@ -1,10 +1,10 @@
 from gemini_torch.model import Gemini
-from gemini_torch.utils import ImgToTransformer, AudioToTransformer
+from gemini_torch.utils import ImgToTransformer, AudioToLangEmbedding
 from gemini_torch.tokenizer import MultimodalSentencePieceTokenizer
 
 __all__ = [
     "Gemini",
     "ImgToTransformer",
-    "AudioToTransformer",
+    "AudioToLangEmbedding",
     "MultimodalSentencePieceTokenizer",
 ]
